fe9a374a2795666e18eecca6ad0512e80758465e
Artem V. Ageev <artem.v.ageev@gmail.com>
2024-11-05 21:50:31 +0200

    fix make.sh

--- make.ps1
+++ make.ps1
@@ -22,43 +22,57 @@ Function PrivWget {
 
 Function PrivMsiexec {
     While ($Input.MoveNext()) {
-        Start-Process -PassThru -Wait -FilePath $Input.Current -ArgumentList '/SP-', '/VERYSILENT', '/SUPPRESSMSGBOXES', '/NORESTART'
+        $params = @{}
+        Switch ((Split-Path -Path $Input.Current -Leaf).Split('.')[-1]) {
+            'msi' {
+                $params = @{
+                    FilePath = 'msiexec'
+                    ArgumentList = '/passive', '/package', $Input.Current
+                }
+            }
+            'exe' {
+                $params = @{
+                    FilePath = $Input.Current
+                    ArgumentList = '/SP-', '/VERYSILENT', '/SUPPRESSMSGBOXES', '/NORESTART'
+                }
+            }
+        }
+        Start-Process -PassThru -Wait @params
         Remove-Item $Input.Current
     }
 }
 
-Function PrivPrepare {
-    $VAR = @(
-        @{
-            Cmd = 'lazbuild'
-            Url = 'https://netix.dl.sourceforge.net/project/lazarus/Lazarus%20Windows%2064%20bits/Lazarus%203.6/lazarus-3.6-fpc-3.2.2-win64.exe?viasf=1'
-            Path = "C:\Lazarus"
-        }
-    )
-    ForEach ($REPLY in $VAR) {
-        If (-not (Get-Command $REPLY.Cmd -ea 'continue')) {
-            PrivWget $REPLY.Url | PrivMsiexec
-            $env:PATH+=";$($REPLY.Path)"
-            Get-Command $REPLY.Cmd
-        }
+Function PrivLazBuild {
+    $VAR = @{
+        Cmd = 'lazbuild'
+        Url = 'https://netix.dl.sourceforge.net/project/lazarus/Lazarus%20Windows%2064%20bits/Lazarus%203.6/lazarus-3.6-fpc-3.2.2-win64.exe?viasf=1'
+        Path = "C:\Lazarus"
     }
-}
-
-Function PrivPackages {
-    If ( Test-Path -Path 'use' ) {
+    If (-not (Get-Command $VAR.Cmd -ea 'continue')) {
+        PrivWget $VAR.Url | PrivMsiexec
+        $env:PATH+=";$($VAR.Path)"
+        Get-Command $VAR.Cmd
+    }
+    If ( Test-Path -Path 'use\components.txt' ) {
         Start-Process -Wait -FilePath 'git' -ArgumentList 'submodule', 'update', '--recursive', '--init'
         Start-Process -Wait -FilePath 'git' -ArgumentList 'submodule', 'update', '--recursive', '--remote'
-    } Else {
-        New-Item -ItemType Directory -Name 'use'
-    }
-    If ($args.count -gt 0) {
-        ForEach ($REPLY in $args) {
-            PrivWget "https://packages.lazarus-ide.org/$($REPLY).zip" | Expand-Archive -DestinationPath "use/$($REPLY).zip" -Force
-            Remove-Item "$($REPLY).zip"
+        Get-Content -Path 'use\components.txt' | ForEach-Object {
+            If (-not (Start-Process -ea 'continue' -Wait -FilePath 'lazbuild' -ArgumentList '--verbose-pkgsearch', $_)) {
+                If (-not (Start-Process -ea 'continue' -Wait -FilePath 'lazbuild' -ArgumentList '--add-package', $_)) {
+                    If (-not (Test-Path -Path 'use\components.txt')) {
+                        $OutFile = PrivWget "https://packages.lazarus-ide.org/$($_).zip"
+                        Expand-Archive -Path $OutFile -DestinationPath "use\$($_)" -Force
+                        Remove-Item $OutFile
+                    }
+                }
+            }
+        }
+        Get-ChildItem -Filter '*.lpk' -Recurse -File –Path 'use' | ForEach-Object {
+            Start-Process -Wait -FilePath 'lazbuild' -ArgumentList '--add-package-link', $_.Name
         }
     }
-    Get-ChildItem -Filter '*.lpk' -Recurse -File –Path 'use' | ForEach-Object {
-        Start-Process -Wait -FilePath 'lazbuild' -ArgumentList '--add-package-link', $_.Name
+    Get-ChildItem -Filter '*.lpi' -Recurse -File –Path 'src' | ForEach-Object {
+        Start-Process -Wait -FilePath 'lazbuild' -ArgumentList '--no-write-project', '--recursive', '--build-mode=release', $_.Name
     }
 }
 
@@ -67,13 +81,9 @@ Function PrivMain {
     Set-PSDebug -Strict -Trace 1
     Invoke-ScriptAnalyzer -EnableExit -Path $PSCommandPath
     If ($args.count -gt 0) {
-        PrivPrepare
         Switch ($args[0]) {
             'build' {
-                PrivPackages 'Rx' 'ZeosDBO'
-                Get-ChildItem -Filter '*.lpi' -Recurse -File –Path 'src' | ForEach-Object {
-                    Start-Process -Wait -FilePath 'lazbuild' -ArgumentList '--no-write-project', '--recursive', '--no-write-project', '--build-mode=release', $_.Name
-                }
+                PrivLazBuild
             }
             Default {
                 PrivClipper
